38cddf2075d462ac084845d0ac0ee965b081cd48
Mansi Varshney <70686638+mansivar21-22@users.noreply.github.com>
2020-12-18 00:35:36 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,14 @@
+#include<stdio.h> //header file include: preprocessor directive
+int main()
+{
+     /* code */  
+    //input     -> process(logic)   -> output    //principle of computer
+    int num;
+    printf("\nhello world!");
+    printf("\nEnter any number");
+    scanf("%d",&num);
+    printf("Your number is=%d",num);   //.= current directory   / = representation of root (inside directory) 
+    return 0;                          //  .exe= file   
+}                                                  
+//function is block of code, which have name and can be execute again and again,
+//It may be possible to pass arguments to function and it can return value if required
